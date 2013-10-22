d3094c7d4a5eca8b91c1ec9b5d3457c4454c7902
Steve Kondik <shade@chemlab.org>
2013-10-21 21:27:42 -0700

    Add guard makefile for allowing simple recovery build
    Change-Id: I6ec438f61cafce69c5083977c4330277f56aa060

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifneq ($(WITH_SIMPLE_RECOVERY),true)
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -206,3 +208,5 @@ include $(commands_recovery_local_path)/su/Android.mk
 include $(commands_recovery_local_path)/voldclient/Android.mk
 include $(commands_recovery_local_path)/romswitcher/Android.mk
 commands_recovery_local_path :=
+
+endif
